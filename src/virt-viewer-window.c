AOSC-Tracking/virt-viewer

commit cfcac9fb9d4d06281a303a564d96a2ddadadc262

Add all accel groups when enabling window mods
Previously, enabling window modifiers would only add accel groups that
had presumably been disabled to the window in question. This was fragile
and caused bad behavior in cases when the criteria for whether or not an
accel group should be enabled changed between the time the groups were
disabled and re-enabled, potentially leading to certain groups never
being re-added. There is no harm in adding accel groups that are already
added to a window, so this change simply eliminates the unnecessary
check.

Signed-off-by: Shawn M. Chapla <schapla@codeweavers.com>

src/virt-viewer-window.c
@@ -896,9 +896,6 @@ virt_viewer_window_enable_modifiers(VirtViewerWindow *self)
 
     /* This allows global accelerators like Ctrl+Q == Quit */
     for (accels = priv->accel_list ; accels ; accels = accels->next) {
-        if (virt_viewer_app_get_enable_accel(priv->app) &&
-            priv->accel_group == accels->data)
-            continue;
         gtk_window_add_accel_group(GTK_WINDOW(priv->window), accels->data);
     }
 

commit bda6dbbecf8627f0696eef974c0bfe5f42727d29

Don't double attach accels on enable window mods
Check whether accels are currently attached to a window before attaching
them when enabling window modifiers.

This commit fixes an assert in _gtk_accel_group_attach introduced by
cfcac9fb, which erroneously assumed that is harmless to re-add accel
groups that have already been added to a window.

Signed-off-by: Shawn M. Chapla <schapla@codeweavers.com>

src/virt-viewer-window.c
@@ -888,15 +888,21 @@ virt_viewer_window_enable_modifiers(VirtViewerWindow *self)
     GSList *accels;
     guint i;
     GtkAccelKey key;
+    GSList *attached_accels;
 
     if (priv->accel_enabled)
         return;
 
     /* This allows F10 activating menu bar */
     g_object_set_property(G_OBJECT(settings), "gtk-menu-bar-accel", &priv->accel_setting);
+    attached_accels = gtk_accel_groups_from_object(G_OBJECT(priv->window));
 
     /* This allows global accelerators like Ctrl+Q == Quit */
     for (accels = priv->accel_list ; accels ; accels = accels->next) {
+        /* Do not attach accels that are already attached. */
+        if (attached_accels && g_slist_find(attached_accels, accels->data))
+            continue;
+
         gtk_window_add_accel_group(GTK_WINDOW(priv->window), accels->data);
     }
 